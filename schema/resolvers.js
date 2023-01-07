cb04a2873a5610c09da25237ac3f362740ffe16d
mauLovera <mauricioloveraedu@gmail.com>
2023-01-07 13:51:33 +0100

    feat: add user route && lodash;

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,17 @@
 const { UserList } = require("../data")
+const _ = require("lodash")
 
 const resolvers = {
   Query: {
     users() {
       return UserList
+    },
+    user(parent, args) {
+      const id = args.id
+      const user = _.find(UserList, {
+        id: Number(id),
+      })
+      return user
     },
   },
 }
