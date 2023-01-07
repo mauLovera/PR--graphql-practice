6ff4a02975a57cfbb6bc1c09014bcd3b28639615
mauLovera <mauricioloveraedu@gmail.com>
2023-01-07 11:57:31 +0100

    feat: add resolvers, typedefs, and data to server.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const UserList = [
+  {
+    id: 1,
+    name: "Mark",
+    username: `Mark123`,
+    age: 18,
+    nationality: "US",
+  },
+  {
+    id: 1,
+    name: "Joan",
+    username: `Joan123`,
+    age: 18,
+    nationality: "US",
+  },
+  {
+    id: 1,
+    name: "John",
+    username: `John123`,
+    age: 18,
+    nationality: "US",
+  },
+]
+
+module.exports = {
+  UserList
+}
